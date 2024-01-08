bed176926d4bea7d9bdd74c2503361eef82fb89d
Priyanka Gaikwad <gaikwadpriyanka1228@gmail.com>
2024-01-09 01:33:54 +0530

    edited files

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(title: 'Jokes App'),
+      home: const HomePage(title: 'Jokes App'),
     );
   }
 }
